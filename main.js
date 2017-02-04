ea5048f2d98e4769e35a275b6dac7b8d362fbfa2
figorix <aqfgit@gmail.com>
2017-02-04 15:24:39 +0100

    block now can be resized on both axis

--- main.js
+++ main.js
@@ -31,7 +31,15 @@ function ManipulatableBoardElement(domElement) {
     x: this.element.getBoundingClientRect().left,
     y: this.element.getBoundingClientRect().top
   };
+  this.stationaryWidth = this.width;
+  this.stationaryHeight = this.height;
   this.selected = false;
+  this.mousedown = false;
+  this.draggable = true;
+  this.resizableX = false;
+  this.resizableY = false;
+  this.draggingEdge = null;
+  this.isBeingResized = false;
 }
 
 //Board doesn't require an instance
@@ -40,64 +48,99 @@ function Board() {}
 
 Board.elements = [];
 
-Board.elementOnMouseDown = function elementOnMouseDown(event, el) {
-  el.selected = true;
-  el.x = Board.mousePos.x;
-  el.dragPoint.x = Board.mousePos.x;
-  el.dragPoint.y = Board.mousePos.y;
-  el.element.style.cursor = '-webkit-grabbing';
-}
-
-Board.elementOnMouseMove = function elementOnMouseMove(event, el) {
-  const isCursorOnTheLeftEdge = (Board.mousePos.x >= el.x - 5) && (Board.mousePos.x <= el.x + 5);
-  const isCursorOnTheRightEdge = (Board.mousePos.x <= el.x + el.width + 5) && (Board.mousePos.x >= el.x + el.width - 5);
-  const isCursorOnTheTopEdge = (Board.mousePos.y >= el.y - 5) && (Board.mousePos.y <= el.y + 5);
-const isCursorOnTheBottomEdge = (Board.mousePos.y <= el.y + el.height + 5) && (Board.mousePos.y >= el.y + el.height - 5);
-
-  if (isCursorOnTheLeftEdge){
-    el.element.style.cursor = 'ew-resize';
-  } else if (isCursorOnTheRightEdge) {
-    el.element.style.cursor = 'ew-resize';
-  } else if (isCursorOnTheTopEdge) {
-    el.element.style.cursor = 'ns-resize';
-  } else if(isCursorOnTheBottomEdge) {
-    el.element.style.cursor = 'ns-resize';
-  } else {
-    el.element.style.cursor = '-webkit-grab';
+Board.moveElement = function moveElement(event, el) {
+  if (el.draggable) {
+    el.selected = true;
+    el.x = Board.mousePos.x;
+    el.dragPoint.x = Board.mousePos.x;
+    el.dragPoint.y = Board.mousePos.y;
+    el.element.style.cursor = '-webkit-grabbing';
   }
 }
 
-Board.windowOnMouseUp = function windowOnMouseUp(event, el) {
+Board.checkIfResizable = function checkIfResizable(event, el) {
+  const isCursorOnTheLeftEdge = (Board.mousePos.x >= el.x - 5) && (Board.mousePos.x <= el.x + 5);
+  const isCursorOnTheRightEdge = (Board.mousePos.x <= el.x + el.width + 5) && (Board.mousePos.x >= el.x + el.width - 5);
+  const isCursorOnTheTopEdge = (Board.mousePos.y >= el.y - 5) && (Board.mousePos.y <= el.y + 5);
+  const isCursorOnTheBottomEdge = (Board.mousePos.y <= el.y + el.height + 5) && (Board.mousePos.y >= el.y + el.height - 5);
+
+  Board.isCursorMoving = true;
+
+  if (isCursorOnTheLeftEdge && !el.isBeingResized){
+    el.element.style.cursor = 'ew-resize';
+    el.draggingEdge = 'left';
+    el.resizableX = true;
+    el.resizableY = false;
+    el.draggable = false;
+  } else if (isCursorOnTheRightEdge && !el.isBeingResized) {
+    el.element.style.cursor = 'ew-resize';
+    el.draggingEdge = 'right';
+    el.resizableX = true;
+    el.resizableY = false
+    el.draggable = false;
+  } else if (isCursorOnTheTopEdge && !el.isBeingResized) {
+    el.element.style.cursor = 'ns-resize';
+    el.draggingEdge = 'top';
+    el.resizableY = true;
+    el.resizableX = false;
+    el.draggable = false;
+  } else if(isCursorOnTheBottomEdge && !el.isBeingResized) {
+    el.element.style.cursor = 'ns-resize';
+    el.draggingEdge = 'bottom';
+    el.resizableY = true;
+    el.resizableX = false;
+    el.draggable = false;
+  } else {
+    el.element.style.cursor = '-webkit-grab';
+    el.draggable = true;
+  }
+}
+
+Board.elementResize = function elementResize(event, el) {
+  if (el.resizableX || el.resizableY === true) {
+    el.dragPoint.x = Board.mousePos.x;
+    el.dragPoint.y = Board.mousePos.y;
+    el.mousedown = true;
+  }
+}
+
+Board.dropElement = function dropElement(event, el) {
   if(el.selected === true) {
     el.y = Board.mousePos.y - (el.dragPoint.y) + el.stationaryPosition.y;
     el.x = Board.mousePos.x - (el.dragPoint.x) + el.stationaryPosition.x;
     el.element.style.cursor = '-webkit-grab';
   }
   el.selected = false;
-  Board.mousedown = false;
+  el.mousedown = false;
   el.dragPoint.x = 0;
   el.dragPoint.y = 0;
   el.stationaryPosition.x = el.x;
   el.stationaryPosition.y = el.y;
+
+  el.stationaryWidth = el.width;
+  el.stationaryHeight = el.height;
+  el.isBeingResized = false;
 }
 
-Board.windowOnMouseDown = function elementOnMouseDown(event, el) {
+Board.elementOnMouseDown = function elementOnMouseDown(event, el) {
   if(el.selected===true){
-      Board.mousedown = true;
+      el.mousedown = true;
     }
 }
 
 Board.updateListeners = function updateListeners() {
   for(let item of Board.elements) {
-    item.element.removeEventListener('mousemove', () => Board.elementOnMouseMove(event, item));
+    item.element.removeEventListener('mousemove', () => Board.checkIfResizable(event, item));
+    item.element.removeEventListener('mousedown', () => Board.moveElement(event, item));
+    window.removeEventListener('mouseup', () => Board.dropElement(event, item));
     item.element.removeEventListener('mousedown', () => Board.elementOnMouseDown(event, item));
-    window.removeEventListener('mouseup', () => Board.windowOnMouseUp(event, item));
-    window.removeEventListener('mousedown', () => Board.windowOnMouseDown(event, item));
+    item.element.removeEventListener('mousedown', () => Board.elementResize(event, item));
 
-    item.element.addEventListener('mousemove', () => Board.elementOnMouseMove(event, item));
+    item.element.addEventListener('mousemove', () => Board.checkIfResizable(event, item));
+    item.element.addEventListener('mousedown', () => Board.moveElement(event, item));
+    window.addEventListener('mouseup', () => Board.dropElement(event, item));
     item.element.addEventListener('mousedown', () => Board.elementOnMouseDown(event, item));
-    window.addEventListener('mouseup', () => Board.windowOnMouseUp(event, item));
-    window.addEventListener('mousedown', () => Board.windowOnMouseDown(event, item));
+    item.element.addEventListener('mousedown', () => Board.elementResize(event, item));
   }
 
 }
@@ -112,18 +155,52 @@ Board.mousePos = {
   x: 0,
   y: 0
 }
-Board.mousedown = false;
+
+Board.isCursorMoving = false;
+
+Board.checkIfCursorIsMoving = null;
 
 Board.update = function update() {
   for (let item of this.elements) {
-    if(Board.mousedown === true && item.selected === true) {
+    if (item.mousedown === true && item.selected === true) {
       item.y = Board.mousePos.y - (item.dragPoint.y) + item.stationaryPosition.y;
       item.x = Board.mousePos.x - (item.dragPoint.x) + item.stationaryPosition.x;
     }
+    else if ((item.resizableX || item.resizableY) && item.mousedown && Board.isCursorMoving) {
+      item.isBeingResized = true;
+      if (item.resizableX) {
+        switch (item.draggingEdge) {
+          case 'right':
+            item.width =  Board.mousePos.x - item.dragPoint.x + item.stationaryWidth;
+            break;
+          case 'left':
+            item.width = item.dragPoint.x - Board.mousePos.x + item.stationaryWidth;
+            if (item.width > 1) item.x = Board.mousePos.x
+            break;
+        }
+      }
+      
+      if (item.resizableY) {
+        switch (item.draggingEdge) {
+          case 'bottom':
+            item.height =  Board.mousePos.y - item.dragPoint.y + item.stationaryHeight;
+            break;
+          case 'top':
+            item.height = item.dragPoint.y - Board.mousePos.y + item.stationaryHeight;
+            if (item.height > 1) item.y = Board.mousePos.y;
+            break;
+        }
+      }
+    }
+ 
+    if (item.width <= 0) item.width = 1;
+    if (item.height <= 0) item.height = 1;
+
     item.element.style.left = item.x + 'px'; 
     item.element.style.top = item.y + 'px';
     item.element.style.width = item.width + 'px';
     item.element.style.height = item.height + 'px';
+    
   }
 }
 
@@ -132,6 +209,11 @@ const sizeInput = document.getElementById('size-input');
 const createObjectButton = document.getElementById('create-element');
 
 window.addEventListener('mousemove', function(event) {
+  clearTimeout(Board.checkIfCursorIsMoving);
+  Board.isCursorMoving = true;
+  Board.checkIfCursorIsMoving = setTimeout(function() {
+    Board.isCursorMoving = false;
+  }, 100)
   Board.mousePos.x = event.clientX;
   Board.mousePos.y = event.clientY;
 })
@@ -139,8 +221,8 @@ window.addEventListener('mousemove', function(event) {
 createObjectButton.addEventListener('click', function(event) {
   const size = sizeInput.value;
 
-  if (size < 50 || size > 200) {
-    alert("Element's size must fall between 50 and 200");
+  if (size < 20 || size > 300) {
+    alert("Element's size must fall between 20 and 300");
     return;
   }
   const element = new DomElement({
